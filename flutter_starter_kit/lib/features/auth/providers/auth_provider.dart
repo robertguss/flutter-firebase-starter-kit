ab75eb522b233baf8a5860870adec74138652edf
Robert Guss <robertguss@gmail.com>
2026-03-06 21:47:49 -0500

    feat: add auth state provider

---
+++ flutter_starter_kit/lib/features/auth/providers/auth_provider.dart
@@ -0,0 +1,12 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_starter_kit/features/auth/services/auth_service.dart';
+
+final authServiceProvider = Provider<AuthService>((ref) {
+  return AuthService();
+});
+
+final authStateProvider = StreamProvider<User?>((ref) {
+  final authService = ref.watch(authServiceProvider);
+  return authService.authStateChanges;
+});
